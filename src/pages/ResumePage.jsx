c791cb0733dc57b6211d1e0a2cca5a52f4ba4ac3
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-06-16 23:50:46 +0100

    set up error page and resume page components

---
+++ src/pages/ResumePage.jsx
@@ -0,0 +1,18 @@
+export default function Resume() {
+    return (
+        <div className="resume">
+            <h2>My Resume:</h2>
+            <img src="../assets/resume.png" alt="resume image"></img>
+            <h2>My Skills:</h2>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>SQL</li>
+                <li>PostgreSQL</li>
+                <li>MongoDB</li>
+            </ul>
+        </div>
+    );
+  }
