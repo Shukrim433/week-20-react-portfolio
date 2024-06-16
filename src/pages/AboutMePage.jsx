519a7dff8cc93ee6571b5e84b6e1af701808ce9b
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-06-16 23:54:55 +0100

    set up about me page

---
+++ src/pages/AboutMePage.jsx
@@ -0,0 +1,8 @@
+export default function AboutMePage() {
+    return (
+        <div className="aboutMe">
+            {/*<img></img>    icon/photo*/}
+            <p>about me text</p>
+        </div>
+    )
+}
