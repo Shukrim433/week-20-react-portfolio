4fceaddea0fdc7d58cbce0aeaec68ed75c1a4225
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-06-16 22:48:09 +0100

    set up my react routes for all the pages in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+// functions to set up routes and use them
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+// import all the pages for this app
+import App from './App';
+import ErrorPage from './pages/ErrorPage';
+import AboutMePage from './pages/AboutMePage'; // default page
+import PortfolioPage from './pages/PortfolioPage';
+import ContactMePage from './pages/ContactMePage';
+import ResumePage from './pages/ResumePage';
+
+
+const router = createBrowserRouter([
+  // define all the accessible routes, and which component(page) responds to which URL
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <AboutMePage />,
+      },
+      {
+        path: 'portfolio',
+        element: <PortfolioPage />,
+      },
+      {
+        path: 'contact',
+        element: <ContactMePage />,
+      },
+      {
+        path: 'resume',
+        element: <ResumePage />,
+      },
+    ],
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
