e8290e4c34a772c5633b841de7995c828abff289
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-06-17 22:36:40 +0100

    added key prop to navbar component

--- src/components/UI/Navbar.jsx
+++ src/components/UI/Navbar.jsx
@@ -4,8 +4,8 @@ export default function Nav({ links }) {
     return (
       <nav className="navbar">
         <ul>
-            {links.map((link) => 
-              <a>{link}</a>
+            {links.map((link, index) => 
+              <a key={index}>{link}</a>  //  Each child in a list should have a unique "key" prop. ERROR-  <a> cannot appear as a descendant of
             )}
         </ul>
       </nav>
