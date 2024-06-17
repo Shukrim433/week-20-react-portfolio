e9843c1b2c384e2a0beeb62640bb65ae764bfedf
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-06-17 22:38:26 +0100

    about me css

--- src/pages/AboutMePage.jsx
+++ src/pages/AboutMePage.jsx
@@ -1,8 +1,11 @@
+import '../styles/About.css';
+
 export default function AboutMePage() {
     return (
         <div className="aboutMe">
             {/*<img></img>    icon/photo*/}
-            <p>about me text</p>
+            <p>My name is Shukri Mohamed, I am a University of birmingham <br>
+            </br> Coding Bootcamp Student.</p>
         </div>
     )
 }
